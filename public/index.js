9a278d9ef7b5a9b693d3e09f3e4bc5c80d9fdf48
[IgnetStudio] <ignet.studio@gmail.com>
2020-11-01 12:33:37 +0100

    [ADD] example Google Map with marker & info window

---
+++ public/index.js
@@ -0,0 +1,29 @@
+// This example displays a marker at Ljubljana, Slovenija.
+// When the user clicks the marker, an info window opens.
+function initMap() {
+  const lj = { lat: 46.056946, lng: 14.505751 };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: lj,
+  });
+  const contentString =
+    '<div id="content">' +
+    '<div id="siteNotice">' +
+    "</div>" +
+    '<h1 id="firstHeading" class="firstHeading">Ljubljana</h1>' +
+    '<div id="bodyContent">' +
+    "<p><b>Slovenija</b>" +
+    "</div>" +
+    "</div>";
+  const infowindow = new google.maps.InfoWindow({
+    content: contentString,
+  });
+  const marker = new google.maps.Marker({
+    position: lj,
+    map,
+    title: "Ljubljana, Slovenija",
+  });
+  marker.addListener("click", () => {
+    infowindow.open(map, marker);
+  });
+}
